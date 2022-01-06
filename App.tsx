b4eb36896447a6eac9f7b221c0b300043ba2ef09
Juan Adon <juan.addon@gmail.com>
2022-01-06 01:53:36 -0400

    Basic counter based on components in react native using typescript, the opacity effect was added to the buttons, Ios and Android has a different type of touchable effects

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { HelloWorldScreen } from './src/screens/HelloWorldScreen';
+import { CounterScreen } from './src/screens/CounterScreen';
 
 export const App = () => {
   return (
-    <HelloWorldScreen/>
+    <CounterScreen/>
   )
 }
